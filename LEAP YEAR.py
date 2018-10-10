ab90cb6475574a318a1cbb003feec4d199375018
AayushTyagi1 <42893909+AayushTyagi1@users.noreply.github.com>
2018-10-10 22:31:27 +0530

    Update LEAP YEAR.py
    we can reduce the amount of if else and make it quite simple

--- LEAP YEAR.py
+++ LEAP YEAR.py
@@ -1,14 +1,7 @@
 #to find if year entered by user is leap year
 
 a=int(input('enter a year:'))
-if (a%4)==0:
-   if (a%100)==0:
-      if (a%400)==0:
+if (((a%4==0)and(a%100!=0))or(a%400==0))):
          print("leap year")
       else:
          print("not leap year")
-   else:
-         print("not leap year")
-         
-else:
-        print("not leap year")   
